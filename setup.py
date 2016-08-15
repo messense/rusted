1dc7bb36e42c46a273cacdab7cc80f039e237b24
messense <messense@icloud.com>
2016-08-16 00:20:55 +0800

    Update install requirements

--- setup.py
+++ setup.py
@@ -9,8 +9,12 @@ setup(
     author_email='messense@icloud.com',
     url='https://github.com/messense/rusted',
     keywords='rust explain error',
-    description='',
+    description='Simple tool to highlight rustc --explain output',
     py_modules=['rusted'],
+    install_requires=[
+        'mistune>=0.7.3',
+        'pygments>=2.1.3',
+    ],
     entry_points={
         'console_scripts': [
             'rusted = rusted:main',
@@ -18,6 +22,16 @@ setup(
     },
     classifiers=[
         'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
         'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
     ]
 )
